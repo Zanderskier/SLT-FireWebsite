b1752348d8897f4457d9a909b47c73c21c6a3204
Emilio <juniorsanchez9060@gmail.com>
2024-10-16 23:26:50 -0700

    deleted unnecessary text from first commit

--- slt-fire-website/src/App.js
+++ slt-fire-website/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Edit <code>src/App.js</code> and save to reload.</p>
         
-        {/* Correcting the paragraph tags */}
+        
         <p>Emilio Sanchez CSUS CSC 190 Fall 2024</p>
 
         <a
